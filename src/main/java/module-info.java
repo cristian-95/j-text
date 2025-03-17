e04759347bfccd84f93568e151dc70954affce7f
cristian-95 <nem.cristianrc@gmail.com>
2025-03-17 18:26:04 -0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.cristian.jtext {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.cristian.jtext to javafx.fxml;
+    exports com.cristian.jtext;
+}
